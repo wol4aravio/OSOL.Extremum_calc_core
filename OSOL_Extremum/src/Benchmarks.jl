c6caf20f8dc078a7d669cdc3262d0579afebc61b
valentin.panovskiy <valentin.panovskiy@skillbox.ru>
2020-05-13 20:21:28 +0300

    ackley_2

--- OSOL_Extremum/src/Benchmarks.jl
+++ OSOL_Extremum/src/Benchmarks.jl
@@ -1,14 +1,24 @@
 module Benchmarks
 
 struct Benchmark
-    f::Any
-    search_area::Any
-    Benchmark(;f, search_area) = new(f, search_area)
+    f
+    search_area
+    solution
+    Benchmark(;f, search_area, solution) = new(f, search_area, solution)
 end
 
-dummy = Benchmark(
-    f = x->x * x,
-    search_area = [[-10 10]]
+###########################################################################
+#                                 2 DIM                                   #
+###########################################################################
+
+ackley_2 = Benchmark(
+    f = function(v)
+        x = v[1]
+        y = v[2]
+        return -200exp(-0.2sqrt(x^2 + y^2))
+    end,
+    search_area = [[-32 32]; [-32 32]],
+    solution = [0; 0]
 )
 
 end
