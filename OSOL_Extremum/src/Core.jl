79df918216f6a7e923776eb460bfb064bb485ef4
valentin.panovskiy <valentin.panovskiy@skillbox.ru>
2020-05-15 09:49:38 +0300

    draft for api: list of problems

--- OSOL_Extremum/src/Core.jl
+++ OSOL_Extremum/src/Core.jl
@@ -1,20 +1,29 @@
+include("./Benchmarks.jl")
+
+using .Benchmarks
 using Bukdu
+
+problems = [
+    Benchmarks.ackley_2,
+    Benchmarks.ackley_3,
+    Benchmarks.adjiman,
+]
 
 struct RESTController <: ApplicationController
     conn::Conn
-end
-
-function index(c::RESTController)
-    return render(JSON, "Hello World")
 end
 
 function health(c::RESTController)
     return render(JSON, "OK")
 end
 
-routes() do
-    get("/",       RESTController, index)
-    get("/health", RESTController, health)
+function list_of_problems(c::RESTController)
+    return render(JSON, [p.name for p in problems])
 end
 
-Bukdu.start(8080)
+routes() do
+    get("/health", RESTController, health)
+    get("/problems", RESTController, list_of_problems)
+end
+
+Bukdu.start(8085)
