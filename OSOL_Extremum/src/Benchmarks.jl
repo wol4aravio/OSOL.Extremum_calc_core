92b4b3803744c5b59cdb52286883ffad5c3ee707
Valentin Panovskiy <panovskiy.v@yandex.ru>
2020-05-16 20:45:56 +0300

    Benchmarks 2d (#1)
    * ackley_2
    
    * formatted
    
    * new func
    
    * draft for api: list of problems
    
    * several benchmarks
    
    * draft
    
    Co-authored-by: valentin.panovskiy <valentin.panovskiy@skillbox.ru>

--- OSOL_Extremum/src/Benchmarks.jl
+++ OSOL_Extremum/src/Benchmarks.jl
@@ -1,14 +1,66 @@
 module Benchmarks
 
+using Zygote
+
 struct Benchmark
-    f::Any
-    search_area::Any
-    Benchmark(;f, search_area) = new(f, search_area)
+    f
+    search_area::Array{Float64,2}
+    solution::Array{Float64,1}
+    name::String
+    Benchmark(; f, search_area, solution, name) = new(f, search_area, solution, name)
+
 end
 
-dummy = Benchmark(
-    f = x->x * x,
-    search_area = [[-10 10]]
+# http://benchmarkfcns.xyz/
+# http://infinity77.net/
+
+################################################################################
+#                                 2 DIM                                        #
+################################################################################
+
+function _ackley_2_func(v)
+    x = v[1]
+    y = v[2]
+    return -200 * exp(-0.2 * sqrt(x^2 + y^2))
+end
+
+ackley_2 = Benchmark(
+    f = _ackley_2_func,
+    search_area = [[-32 32]; [-32 32]],
+    solution = [0; 0],
+    name = "AckleyNum2",
 )
+
+################################################################################
+
+function _ackley_3_func(v)
+    x = v[1]
+    y = v[2]
+    return -200 * exp(-0.2 * sqrt(x^2 + y^2)) + 5 * exp(cos(3x) + sin(3y))
+end
+
+ackley_3 = Benchmark(
+    f = _ackley_3_func,
+    search_area = [[-32 32]; [-32 32]],
+    solution = [0.682584587365898; -0.36075325513719],
+    name = "AckleyNum3",
+)
+
+################################################################################
+
+function _adjiman_func(v)
+    x = v[1]
+    y = v[2]
+    return cos(x)sin(y) - x/(y^2 + 1)
+end
+
+adjiman = Benchmark(
+    f = _adjiman_func,
+    search_area = [[-1 2]; [-1 1]],
+    solution = [2; 0.10578],
+    name = "Adjiman",
+)
+
+################################################################################
 
 end
--- OSOL_Extremum/src/Core.jl
+++ OSOL_Extremum/src/Core.jl
@@ -1,20 +1,46 @@
+include("./Benchmarks.jl")
+
+using .Benchmarks
 using Bukdu
+
+problems = [
+    Benchmarks.ackley_2,
+    Benchmarks.ackley_3,
+    Benchmarks.adjiman,
+]
 
 struct RESTController <: ApplicationController
     conn::Conn
-end
-
-function index(c::RESTController)
-    return render(JSON, "Hello World")
 end
 
 function health(c::RESTController)
     return render(JSON, "OK")
 end
 
-routes() do
-    get("/",       RESTController, index)
-    get("/health", RESTController, health)
+function list_of_problems(c::RESTController)
+    return render(JSON, [p.name for p in problems])
 end
 
-Bukdu.start(8080)
+routes() do
+    get("/health", RESTController, health)
+    get("/problems", RESTController, list_of_problems)
+    for p in problems
+        name = p.name
+        function problem_call(c::RESTController)
+            @info :payload (c.params.x)
+            return render(JSON, p.f(c.params.x))
+        end
+        post("/call_f/$name", RESTController, problem_call)
+    end
+    for p in problems
+        name = p.name
+        function problem_call(c::RESTController)
+            @info :payload (c.params.x)
+            return render(JSON, p.f'(c.params.x))
+        end
+        post("/call_f_grad/$name", RESTController, problem_call)
+    end
+    plug(Plug.Parsers, [:json])
+end
+
+Bukdu.start(8085)
