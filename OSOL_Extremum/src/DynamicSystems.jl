c94c1104f13399a35465f8bdd69f88b94503e6a0
wol4aravio <panovskiy.v@yandex.ru>
2020-06-13 16:15:37 +0300

    added satellite stabilization

--- OSOL_Extremum/src/DynamicSystems.jl
+++ OSOL_Extremum/src/DynamicSystems.jl
@@ -2,9 +2,9 @@ module DynamicSystems
 
 using DifferentialEquations
 
-function ode_satellite_orientation(x, p, t)
+function ode_satellite_orientation(x, params, t)
     x₁, x₂ = x
-    u = p[1]
+    u = params[1]
     return [
         x₂,
         u(t, x),
@@ -12,12 +12,25 @@ function ode_satellite_orientation(x, p, t)
     ]
 end
 
+function ode_satellite_stabilization(x, params, t)
+    p, q, r = x
+    u₁, u₂, u₃ = [c(t, x) for c in params]
+    return [
+        u₁ / 6,
+        u₂ - 0.2 * p * r,
+        0.2 * (u₃ + p * q),
+        sum((u₁, u₂, u₃) .|> abs)
+    ]
+end
+
 DS = Dict()
 function create_ds(problem_name)
-    if problem_name == "satellite_orientation"
-        global DS
-        if problem_name ∉ keys(DS)
+    global DS
+    if problem_name ∉ keys(DS)
+        if problem_name == "satellite_orientation"
             DS[problem_name] = ODEProblem(ode_satellite_orientation, nothing, (0.0, 1.0), p=nothing)
+        elseif problem_name == "satellite_stabilization"
+            DS[problem_name] = ODEProblem(ode_satellite_stabilization, nothing, (0.0, 1.0), p=nothing)
         end
     end
 end
